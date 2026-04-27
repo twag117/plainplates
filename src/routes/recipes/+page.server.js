1d3df28c966b109514e394ec53da3284998de3fe
Travis <twag117@gmail.com>
2026-04-26 20:41:31 -0500

    added category filtering

--- src/routes/recipes/+page.server.js
+++ src/routes/recipes/+page.server.js
@@ -1,18 +1,29 @@
 export async function load({ platform, url }) {
   // do server stuff here
   const search = await url.searchParams.get('search')
+  const category = await url.searchParams.get('category')
   let result
-  if (!search) {
-    result = await platform.env.DB.prepare("SELECT * FROM recipes LIMIT 100").all()   
-  } else {
+
+  if (search && category) {
+    result = await platform.env.DB.prepare(
+      "SELECT * FROM recipes WHERE category = ? AND (name LIKE ? OR description LIKE ?) LIMIT 100"
+    ).bind(`${category}`, `%${search}%`, `%${search}%`).all()
+  } else if (search) {
     result = await platform.env.DB.prepare(
       "SELECT * FROM recipes WHERE name LIKE ? OR description LIKE ? LIMIT 100"
     ).bind(`%${search}%`, `%${search}%`).all()
+  } else if (category) {
+    result = await platform.env.DB.prepare(
+      "SELECT * FROM recipes WHERE category = ? LIMIT 100"
+    ).bind(`${category}`).all()
+  } else {
+    result = await platform.env.DB.prepare("SELECT * FROM recipes LIMIT 100").all()   
   }
-  
+
 
   return {
     // return data here
-    recipes: result.results
+    recipes: result.results,
+    categoryParam: category
   }
 }
--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -7,13 +7,30 @@
 <h1>Explore PlainPlates!</h1>
 
 <hr>
-<div class="search-group">
-  <h3>SEARCH:</h3>
-  <form action="/recipes" method="get">
+
+<form action="/recipes" method="get">
+  <div class="filter-group">
+    <h3 class="form-header">SEARCH:</h3>
     <input type="text" name="search">
-    <input type="submit" value="submit">
-  </form>
-</div>
+  </div>
+
+  <div>
+    <h3 class="form-header">CATEGORY:</h3>
+    <input type="radio" id="category1" name="category" value="" checked={data.categoryParam===null}>
+    <label for="category1">All</label><br>
+    <input type="radio" id="category2" name="category" value="main" checked={data.categoryParam==='main'}>
+    <label for="category2">Main</label><br>
+    <input type="radio" id="category3" name="category" value="side" checked={data.categoryParam==='side'}>
+    <label for="category3">Side</label><br>
+    <input type="radio" id="category4" name="category" value="drink" checked={data.categoryParam==='drink'}>
+    <label for="category4">Drink</label><br>
+    <input type="radio" id="category5" name="category" value="dessert" checked={data.categoryParam==='dessert'}>
+    <label for="category5">Dessert</label><br><br>
+  </div>
+
+  <input type="submit" value="submit">
+</form>
+<br>
 
 <RecipesList recipes={data.recipes} />
 
@@ -23,9 +40,13 @@
   hr {
     border-color: var(--color-accent);
   }
-  .search-group {
+  .filter-group {
     display: flex;
     align-items: center;
     justify-content: flex-start;
+  }
+  .form-header {
+    margin-block-start: 10px;
+    margin-block-end: 10px;
   }
 </style>
